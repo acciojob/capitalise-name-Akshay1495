9ba41ac7cb5ea2ab82032a40fff3294ba7ff49a8
Akshay <103642286+Akshay1495@users.noreply.github.com>
2024-05-22 23:09:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+window.onload = function() {
+    var inputField = document.getElementById('fname');
+    inputField.addEventListener('blur', function() {
+        inputField.value = inputField.value.toUpperCase();
+    });
+};
